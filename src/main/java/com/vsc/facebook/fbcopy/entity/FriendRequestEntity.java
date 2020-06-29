d84127970679a8bff00e6a1e889b67cab5e1fa55
Цветослав Тодоров <gavril3vr@gmail.com>
2020-06-29 21:19:10 +0300

    status

--- src/main/java/com/vsc/facebook/fbcopy/entity/FriendRequestEntity.java
+++ src/main/java/com/vsc/facebook/fbcopy/entity/FriendRequestEntity.java
@@ -1,5 +1,7 @@
 package com.vsc.facebook.fbcopy.entity;
 
+import com.vsc.facebook.fbcopy.enumaration.FriendRequestStatus;
+
 import javax.persistence.*;
 
 @Entity
@@ -9,4 +11,28 @@ public class FriendRequestEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    @Enumerated
+    private FriendRequestStatus status;
+
+    public FriendRequestEntity() {
+        this.id = id;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public FriendRequestStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(FriendRequestStatus status) {
+        this.status = status;
+    }
 }
