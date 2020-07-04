bf44670d1dc218d50768efeac90904fadb137eed
HristoTsekov <xtsekooov@gmail.com>
2020-07-04 18:21:31 +0300

    create post repository

--- src/main/java/com/vsc/facebook/fbcopy/repository/PostRepository.java
+++ src/main/java/com/vsc/facebook/fbcopy/repository/PostRepository.java
@@ -2,12 +2,13 @@ package com.vsc.facebook.fbcopy.repository;
 
 import com.vsc.facebook.fbcopy.entity.Post;
 import com.vsc.facebook.fbcopy.entity.Role;
+import com.vsc.facebook.fbcopy.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface PostRepository extends JpaRepository {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
