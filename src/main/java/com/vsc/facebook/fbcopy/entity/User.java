7f87091c8313e59957865129f22c805e54b65e81
HristoTsekov <xtsekooov@gmail.com>
2020-07-04 18:27:09 +0300

    add firstname,lastname and age in UserEntity

--- src/main/java/com/vsc/facebook/fbcopy/entity/User.java
+++ src/main/java/com/vsc/facebook/fbcopy/entity/User.java
@@ -23,6 +23,16 @@ public class User implements UserDetails {
     @Column(name = "is active",nullable = false,unique = true)
     private boolean isActive;
 
+
+    @Column(name = "first_name", nullable = false, unique = true)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, unique = true)
+    private String lastName;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+
     @ManyToMany
     private Set<Role> authorities;
 
@@ -55,6 +65,30 @@ public class User implements UserDetails {
 
     public User() {
 
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
     public long getId() {
