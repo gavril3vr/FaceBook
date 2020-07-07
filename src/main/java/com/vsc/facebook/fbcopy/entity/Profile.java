bf690701a0f06acea18d5cd1ad9a37e6d3bf0815
Nancy <gnr.axlrose.6.2.1962@gmail.com>
2020-07-07 13:05:51 +0300

    ProfileEntityCommit
    commit for profile entity

--- src/main/java/com/vsc/facebook/fbcopy/entity/Profile.java
+++ src/main/java/com/vsc/facebook/fbcopy/entity/Profile.java
@@ -1,11 +1,84 @@
-package com.vsc.facebook.fbcopy.entity;
+package facebook.entity;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
-@Table(name="profiles")
-public class Profile {
+@Table(name = "post")
+public class Post {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private long userId;
+
+    @Column(name = "image_id", nullable = false)
+    private long imageId;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "number_of_likes", nullable = false)
+    private long numberOfLikes;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
+    private User poster;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getPoster() {
+        return poster;
+    }
+
+    public void setPoster(User poster) {
+        this.poster = poster;
+    }
+
+    public Post() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(long imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public long getNumberOfLikes() {
+        return numberOfLikes;
+    }
+
+    public void setNumberOfLikes(long numberOfLikes) {
+        this.numberOfLikes = numberOfLikes;
+    }
+
 }
